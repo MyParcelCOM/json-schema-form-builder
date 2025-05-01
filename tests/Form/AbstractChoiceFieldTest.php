2e732593fd986578410771f15b065039a839b154
Daniel Verner <daniel@myparcel.com>
2025-05-01 16:45:32 +0200

    Added more tests for fields

---
+++ tests/Form/AbstractChoiceFieldTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Form;
+
+use Faker\Factory;
+use InvalidArgumentException;
+use MyParcelCom\JsonSchema\FormBuilder\Form\AbstractChoiceField;
+use MyParcelCom\JsonSchema\FormBuilder\Form\ChoiceFieldType;
+use MyParcelCom\JsonSchema\FormBuilder\Form\Option;
+use MyParcelCom\JsonSchema\FormBuilder\Form\OptionCollection;
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
+class AbstractChoiceFieldTest extends TestCase
+{
+    public function test_it_converts_a_select_property_into_an_array(): void
+    {
+        $faker = Factory::create();
+
+        $name = $faker->word();
+        $label = $faker->words(asText: true);
+
+        $select = new class(
+            name: $name,
+            label: $label,
+            options: new OptionCollection(
+                new Option('1'),
+                new Option('2'),
+                new Option('3'),
+            ),
+        ) extends AbstractChoiceField {
+            protected ChoiceFieldType $fieldType = ChoiceFieldType::SELECT;
+        };
+
+        assertEquals([
+            $name => [
+                'type'        => 'string',
+                'description' => $label,
+                'enum'        => ['1', '2', '3'],
+                'meta'        => ['field_type' => 'select'],
+            ],
+        ], $select->toJsonSchemaProperty()->toArray());
+    }
+
+    public function test_it_throws_an_invalid_argument_exception_without_options(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Radio field property requires at least one option.');
+
+        $faker = Factory::create();
+
+        new class(
+            name: $faker->word,
+            label: $faker->words(asText: true),
+            options: new OptionCollection(),
+        ) extends AbstractChoiceField {
+            protected ChoiceFieldType $fieldType = ChoiceFieldType::RADIO;
+        };
+    }
+}
--- tests/Form/SelectTest.php
+++ tests/Form/SelectTest.php
@@ -9,13 +9,15 @@ use InvalidArgumentException;
 use MyParcelCom\JsonSchema\FormBuilder\Form\Option;
 use MyParcelCom\JsonSchema\FormBuilder\Form\OptionCollection;
 use MyParcelCom\JsonSchema\FormBuilder\Form\Select;
+use MyParcelCom\JsonSchema\FormBuilder\Translations\LabelTranslation;
+use MyParcelCom\JsonSchema\FormBuilder\Translations\Locale;
 use PHPUnit\Framework\TestCase;
 
 use function PHPUnit\Framework\assertEquals;
 
 class SelectTest extends TestCase
 {
-    public function test_it_converts_a_select_property_into_an_array(): void
+    public function test_it_converts_into_an_array_basic(): void
     {
         $faker = Factory::create();
 
@@ -40,6 +42,103 @@ class SelectTest extends TestCase
                 'meta'        => ['field_type' => 'select'],
             ],
         ], $select->toJsonSchemaProperty()->toArray());
+    }
+
+    public function test_it_converts_into_an_array_with_enum_labels(): void
+    {
+        $faker = Factory::create();
+
+        $name = $faker->word();
+        $label = $faker->words(asText: true);
+
+        $radio = new Select(
+            name: $name,
+            label: $label,
+            options: new OptionCollection(
+                new Option('a', 'A'),
+                new Option('b', 'B'),
+                new Option('c', 'B'),
+            ),
+        );
+
+        assertEquals([
+            $name => [
+                'type'        => 'string',
+                'description' => $label,
+                'enum'        => ['a', 'b', 'c'],
+                'meta'        => [
+                    'field_type'  => 'select',
+                    'enum_labels' => [
+                        'a' => 'A',
+                        'b' => 'B',
+                        'c' => 'B',
+                    ],
+                ],
+            ],
+        ], $radio->toJsonSchemaProperty()->toArray());
+    }
+
+    public function test_it_converts_into_an_array_with_enum_labels_and_translations(): void
+    {
+        $faker = Factory::create();
+
+        $name = $faker->word();
+        $label = $faker->words(asText: true);
+
+        $radio = new Select(
+            name: $name,
+            label: $label,
+            options: new OptionCollection(
+                new Option('a', 'A', [
+                    new LabelTranslation(locale: Locale::EN_GB, label: 'A'),
+                    new LabelTranslation(locale: Locale::NL_NL, label: 'A in het Nederlands'),
+                    new LabelTranslation(locale: Locale::DE_DE, label: 'A in Deutsch'),
+                ]),
+                new Option('b', 'B', [
+                    new LabelTranslation(locale: Locale::EN_GB, label: 'B'),
+                    new LabelTranslation(locale: Locale::NL_NL, label: 'B in het Nederlands'),
+                    new LabelTranslation(locale: Locale::DE_DE, label: 'B in Deutsch'),
+                ]),
+                new Option('c', 'C', [
+                    new LabelTranslation(locale: Locale::EN_GB, label: 'C'),
+                    new LabelTranslation(locale: Locale::NL_NL, label: 'C in het Nederlands'),
+                    new LabelTranslation(locale: Locale::DE_DE, label: 'C in Deutsch'),
+                ]),
+            ),
+        );
+
+        assertEquals([
+            $name => [
+                'type'        => 'string',
+                'description' => $label,
+                'enum'        => ['a', 'b', 'c'],
+                'meta'        => [
+                    'field_type'              => 'select',
+                    'enum_labels'             => [
+                        'a' => 'A',
+                        'b' => 'B',
+                        'c' => 'C',
+                    ],
+                    'enum_label_translations' => [
+                        'a' => [
+                            'en-GB' => 'A',
+                            'nl-NL' => 'A in het Nederlands',
+                            'de-DE' => 'A in Deutsch',
+                        ],
+                        'b' => [
+                            'en-GB' => 'B',
+                            'nl-NL' => 'B in het Nederlands',
+                            'de-DE' => 'B in Deutsch',
+                        ],
+                        'c' => [
+                            'en-GB' => 'C',
+                            'nl-NL' => 'C in het Nederlands',
+                            'de-DE' => 'C in Deutsch',
+                        ],
+                    ],
+                ],
+            ],
+        ], $radio->toJsonSchemaProperty()->toArray());
     }
 
     public function test_it_throws_an_invalid_argument_exception_without_options(): void
