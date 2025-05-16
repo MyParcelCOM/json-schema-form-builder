e1f8786e6e977660b8ee14b2d40a0bc086c9880c
Daniel Verner <daniel@myparcel.com>
2025-05-16 14:13:33 +0200

    Simplified FormValidationException.php

--- src/Form/Exceptions/FormValidationException.php
+++ src/Form/Exceptions/FormValidationException.php
@@ -11,9 +11,8 @@ class FormValidationException extends Exception
 {
     public function __construct(
         string $message = 'Form validation failed',
-        int $code = 0,
         ?Throwable $previous = null,
     ) {
-        parent::__construct($message, $code, $previous);
+        parent::__construct(message: $message, previous: $previous);
     }
 }
